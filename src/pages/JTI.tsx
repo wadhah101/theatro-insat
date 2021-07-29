090638b2a3892e5a8656a8da3ab89076978e3327
wadhah101 <wadhah.mahroug15@gmail.com>
2021-07-29 22:15:18 +0100

    feat(app): added pages

---
+++ src/pages/JTI.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageProps } from 'gatsby';
+
+const Page: React.FC<PageProps> = () => (
+  <main className="min-h-screen bg-black ">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci vel
+    repellat veniam placeat totam eos, doloribus assumenda odit repudiandae
+    autem ab eum voluptatum. Dolor odio in accusantium labore amet quae!
+  </main>
+);
+
+export default Page;
